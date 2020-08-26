3aa4940eb3d5d4540b922a18db83ffe6d93d2b60
aokad <aokada@ncc.go.jp>
2020-08-26 16:35:31 +0900

    rename R2.fastq

--- scripts/gcat_workflow/rna/resource/sra_fastq_dump.py
+++ scripts/gcat_workflow/rna/resource/sra_fastq_dump.py
@@ -3,7 +3,7 @@
 import gcat_workflow.core.stage_task_abc as stage_task
 
 OUTPUT_FQ1_FORMAT = "sra_fastq_dump/{sample}/1_1.fasq"
-OUTPUT_FQ2_FORMAT = "sra_fastq_dump/{sample}/2_1.fasq"
+OUTPUT_FQ2_FORMAT = "sra_fastq_dump/{sample}/1_2.fasq"
 
 class SRA_fastq_dump(stage_task.Stage_task):
     def __init__(self, params):
@@ -25,24 +25,21 @@ set -x
 mkdir -p {OUTPUT_DIR}/temp
 cd {OUTPUT_DIR}/temp
 
-#fastq-dump -v --split-files {RUN_ID} -O {OUTPUT_DIR}/temp
-# SRR628583 -O /data/SRR628583 
-
 prefetch --max-size 100000000 {RUN_ID}
 ls -l {RUN_ID}/
 fasterq-dump -v --split-files {RUN_ID}/{RUN_ID}.sra
 rm -rf {RUN_ID}
 
 if [ -e {RUN_ID}.sra_1.fastq ]; then mv {RUN_ID}.sra_1.fastq 1_1.fastq; fi
-if [ -e {RUN_ID}.sra_2.fastq ]; then mv {RUN_ID}.sra_2.fastq 2_1.fastq; fi
+if [ -e {RUN_ID}.sra_2.fastq ]; then mv {RUN_ID}.sra_2.fastq 1_2.fastq; fi
 if [ -e {RUN_ID}.sra.fastq ]; then mv {RUN_ID}.sra.fastq 1_1.fastq; fi
 
 # squeeze
 seqtk squeeze {OUTPUT_DIR}/temp/1_1.fastq > {OUTPUT_DIR}/1_1.fastq
 
-if [ -e {OUTPUT_DIR}/temp/2_1.fastq ]
+if [ -e {OUTPUT_DIR}/temp/1_2.fastq ]
 then
-    seqtk squeeze {OUTPUT_DIR}/temp/2_1.fastq > {OUTPUT_DIR}/2_1.fastq
+    seqtk squeeze {OUTPUT_DIR}/temp/1_2.fastq > {OUTPUT_DIR}/1_2.fastq
 fi
 
 cd {OUTPUT_DIR}
@@ -73,7 +70,7 @@ def configure(gcat_conf, run_conf, sample_conf):
         output_fastqs[sample] = [[f1_name], [f2_name]]
 
         arguments = {
-            "RUN_ID": sample_conf.sra_fastq_dump[sample][1],
+            "RUN_ID": sample_conf.sra_fastq_dump[sample],
             "OUTPUT_DIR": output_dir,
             "pass_file": output_dir + '/pass.txt'
         }
