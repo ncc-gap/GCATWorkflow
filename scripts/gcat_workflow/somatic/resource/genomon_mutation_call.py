27d330ffc3491a2af03bd518cd2f8fa380c99efc
ken0-1n <kchiba@hgc.jp>
2021-08-19 12:24:15 +0900

    Modify GenomonMutation to output in VCF

--- scripts/gcat_workflow/somatic/resource/genomon_mutation_call.py
+++ scripts/gcat_workflow/somatic/resource/genomon_mutation_call.py
@@ -31,55 +31,68 @@ fi
 # print_header
 mut_header=""
 
-if [ _{SAMPLE2} = "_None" ]; then 
+ext=""
+if [ _{OUTPUT_FORMAT} = "_vcf" ]; then
+   ext=vcf
+fi
+
+if [ _{SAMPLE2} = "_None" ]; then
 
   # Fisher's Exact Test
-  fisher single -o {OUTPUT_PREF}.fisher_mutations.txt --ref_fa {REFERENCE} -1 {INPUT_BAM1} --samtools_path ${{SAMTOOLS}} {FISHER_SINGLE_OPTION} ${{interval_option}} --samtools_params "{FISHER_SAMTOOLS_OPTION}"
+  fisher single -o {OUTPUT_PREF}.fisher_mutations.${{ext}} -O {OUTPUT_FORMAT} --ref_fa {REFERENCE} -a {SAMPLE1} -1 {INPUT_BAM1} --samtools_path ${{SAMTOOLS}} {FISHER_SINGLE_OPTION} ${{interval_option}} --samtools_params "{FISHER_SAMTOOLS_OPTION}"
 
   # Local realignment using edlib. The candidate mutations are varidated.
-  mutfilter realignment --target_mutation_file {OUTPUT_PREF}.fisher_mutations.txt -1 {INPUT_BAM1} --output {OUTPUT_PREF}.realignment_mutations.txt --ref_genome {REFERENCE}  {REALIGNMENT_OPTION}
+  mutfilter realignment --target_mutation_file {OUTPUT_PREF}.fisher_mutations.${{ext}} -O {OUTPUT_FORMAT} -A {SAMPLE1} -1 {INPUT_BAM1} --output {OUTPUT_PREF}.realignment_mutations.${{ext}} --ref_genome {REFERENCE}  {REALIGNMENT_OPTION}
 
   # Annotation if the candidate is on the simplerepeat. 
-  mutfilter simplerepeat --target_mutation_file {OUTPUT_PREF}.realignment_mutations.txt --output {OUTPUT_PREF}.simplerepeat_mutations.txt --simple_repeat_db {ANNOTATION_DB}/simpleRepeat.bed.gz
+  mutfilter simplerepeat --target_mutation_file {OUTPUT_PREF}.realignment_mutations.${{ext}} -O {OUTPUT_FORMAT} --output {OUTPUT_PREF}.simplerepeat_mutations.${{ext}} --simple_repeat_db {ANNOTATION_DB}/simpleRepeat.bed.gz
 
   # header
-  mut_header="Chr,Start,End,Ref,Alt,depth,variantNum,bases,A_C_G_T,misRate,strandRatio,10%_posterior_quantile,posterior_mean,90%_posterior_quantile,readPairNum,variantPairNum,otherPairNum,10%_posterior_quantile(realignment),posterior_mean(realignment),90%_posterior_quantile(realignment),simple_repeat_pos,simple_repeat_seq"
-  print_header=`echo ${{mut_header}} | tr "," "\t"`
-  echo "${{print_header}}" > {OUTPUT_PREF}.genomon_mutation.result.txt
-  cat {OUTPUT_PREF}.simplerepeat_mutations.txt >> {OUTPUT_PREF}.genomon_mutation.result.txt
+  if [ _{OUTPUT_FORMAT} != "_vcf" ]; then
+    mut_header="Chr,Start,End,Ref,Alt,depth,variantNum,bases,A_C_G_T,misRate,strandRatio,10%_posterior_quantile,posterior_mean,90%_posterior_quantile,readPairNum,variantPairNum,otherPairNum,10%_posterior_quantile(realignment),posterior_mean(realignment),90%_posterior_quantile(realignment),simple_repeat_pos,simple_repeat_seq"
+    print_header=`echo ${{mut_header}} | tr "," "\t"`
+    echo "${{print_header}}" > {OUTPUT_PREF}.genomon_mutation.result.${{ext}}
+    cat {OUTPUT_PREF}.simplerepeat_mutations.${{ext}} >> {OUTPUT_PREF}.genomon_mutation.result.${{ext}}
+  else
+    cp {OUTPUT_PREF}.simplerepeat_mutations.${{ext}} {OUTPUT_PREF}.genomon_mutation.result.${{ext}}
+  fi
 
-  mutil filter -i {OUTPUT_PREF}.genomon_mutation.result.txt -o {OUTPUT_PREF}.genomon_mutation.result.filt.txt {FILTER_SINGLE_OPTION}
+  mutil filter -i {OUTPUT_PREF}.genomon_mutation.result.${{ext}} -O {OUTPUT_FORMAT} -1 {SAMPLE1} -o {OUTPUT_PREF}.genomon_mutation.result.filt.${{ext}} {FILTER_SINGLE_OPTION}
 
 else
   # Fisher's Exact Test
-  fisher comparison -o {OUTPUT_PREF}.fisher_mutations.txt --ref_fa {REFERENCE} -1 {INPUT_BAM1} -2 {INPUT_BAM2} --samtools_path ${{SAMTOOLS}} {FISHER_PAIR_OPTION} ${{interval_option}} --samtools_params "{FISHER_SAMTOOLS_OPTION}"
+  # fisher comparison -o {OUTPUT_PREF}.fisher_mutations.${{ext}} -O {OUTPUT_FORMAT} --ref_fa {REFERENCE} -a {SAMPLE1} -b {SAMPLE2} -1 {INPUT_BAM1} -2 {INPUT_BAM2} --samtools_path ${{SAMTOOLS}} {FISHER_PAIR_OPTION} ${{interval_option}} --samtools_params "{FISHER_SAMTOOLS_OPTION}"
 
   # Local realignment using blat. The candidate mutations are varidated.
-  mutfilter realignment --target_mutation_file {OUTPUT_PREF}.fisher_mutations.txt -1 {INPUT_BAM1} -2 {INPUT_BAM2} --output {OUTPUT_PREF}.realignment_mutations.txt --ref_genome {REFERENCE} {REALIGNMENT_OPTION}
+  mutfilter realignment --target_mutation_file {OUTPUT_PREF}.fisher_mutations.${{ext}} -O {OUTPUT_FORMAT} -A {SAMPLE1} -B {SAMPLE2} -1 {INPUT_BAM1} -2 {INPUT_BAM2} --output {OUTPUT_PREF}.realignment_mutations.${{ext}} --ref_genome {REFERENCE} {REALIGNMENT_OPTION}
 
   # Annotation if the candidate is near Indel. 
-  mutfilter indel --target_mutation_file {OUTPUT_PREF}.realignment_mutations.txt -2 {INPUT_BAM2} --output {OUTPUT_PREF}.indel_mutations.txt --ref_genome {REFERENCE} --samtools_path ${{SAMTOOLS}} {INDEL_OPTION} --samtools_params "{INDEL_SAMTOOLS_OPTION}"
+  mutfilter indel --target_mutation_file {OUTPUT_PREF}.realignment_mutations.${{ext}} -O {OUTPUT_FORMAT} -A {SAMPLE1} -B {SAMPLE2} -2 {INPUT_BAM2} --output {OUTPUT_PREF}.indel_mutations.${{ext}} --ref_genome {REFERENCE} --samtools_path ${{SAMTOOLS}} {INDEL_OPTION} --samtools_params "{INDEL_SAMTOOLS_OPTION}"
 
   # Annotation if the candidate is near the breakpoint. 
-  mutfilter breakpoint --target_mutation_file {OUTPUT_PREF}.indel_mutations.txt -2 {INPUT_BAM2} --output {OUTPUT_PREF}.breakpoint_mutations.txt --ref_genome {REFERENCE} {BREAKPOINT_OPTION}
+  mutfilter breakpoint --target_mutation_file {OUTPUT_PREF}.indel_mutations.${{ext}} -O {OUTPUT_FORMAT} -A {SAMPLE1} -B {SAMPLE2} -2 {INPUT_BAM2} --output {OUTPUT_PREF}.breakpoint_mutations.${{ext}} --ref_genome {REFERENCE} {BREAKPOINT_OPTION}
 
   # Annotation if the candidate is on the simplerepeat. 
-  mutfilter simplerepeat --target_mutation_file {OUTPUT_PREF}.breakpoint_mutations.txt --output {OUTPUT_PREF}.simplerepeat_mutations.txt --simple_repeat_db {ANNOTATION_DB}/simpleRepeat.bed.gz 
+  mutfilter simplerepeat --target_mutation_file {OUTPUT_PREF}.breakpoint_mutations.${{ext}} -O {OUTPUT_FORMAT} --output {OUTPUT_PREF}.simplerepeat_mutations.${{ext}} --simple_repeat_db {ANNOTATION_DB}/simpleRepeat.bed.gz 
 
   # header
-  mut_header="Chr,Start,End,Ref,Alt,depth_tumor,variantNum_tumor,depth_normal,variantNum_normal,bases_tumor,bases_normal,A_C_G_T_tumor,A_C_G_T_normal,misRate_tumor,strandRatio_tumor,misRate_normal,strandRatio_normal,P-value(fisher),readPairNum_tumor,variantPairNum_tumor,otherPairNum_tumor,readPairNum_normal,variantPairNum_normal,otherPairNum_normal,P-value(fisher_realignment),indel_mismatch_count,indel_mismatch_rate,bp_mismatch_count,distance_from_breakpoint,simple_repeat_pos,simple_repeat_seq"
-  print_header=`echo ${{mut_header}} | tr "," "\t"`
-  echo "${{print_header}}" > {OUTPUT_PREF}.genomon_mutation.result.txt
-  cat {OUTPUT_PREF}.simplerepeat_mutations.txt >> {OUTPUT_PREF}.genomon_mutation.result.txt
+  if [ _{OUTPUT_FORMAT} != "_vcf" ]; then
+    mut_header="Chr,Start,End,Ref,Alt,depth_tumor,variantNum_tumor,depth_normal,variantNum_normal,bases_tumor,bases_normal,A_C_G_T_tumor,A_C_G_T_normal,misRate_tumor,strandRatio_tumor,misRate_normal,strandRatio_normal,P-value(fisher),readPairNum_tumor,variantPairNum_tumor,otherPairNum_tumor,readPairNum_normal,variantPairNum_normal,otherPairNum_normal,P-value(fisher_realignment),indel_mismatch_count,indel_mismatch_rate,bp_mismatch_count,distance_from_breakpoint,simple_repeat_pos,simple_repeat_seq"
+    print_header=`echo ${{mut_header}} | tr "," "\t"`
+    echo "${{print_header}}" > {OUTPUT_PREF}.genomon_mutation.result.${{ext}}
+    cat {OUTPUT_PREF}.simplerepeat_mutations.${{ext}} >> {OUTPUT_PREF}.genomon_mutation.result.${{ext}}
+  else
+    cp {OUTPUT_PREF}.simplerepeat_mutations.${{ext}} {OUTPUT_PREF}.genomon_mutation.result.${{ext}}
+  fi
 
-  mutil filter -i {OUTPUT_PREF}.genomon_mutation.result.txt -o {OUTPUT_PREF}.genomon_mutation.result.filt.txt {FILTER_PAIR_OPTION}
+  mutil filter -i {OUTPUT_PREF}.genomon_mutation.result.${{ext}} -O {OUTPUT_FORMAT} -o {OUTPUT_PREF}.genomon_mutation.result.filt.${{ext}} {FILTER_PAIR_OPTION}
 fi 
 
-rm -f {OUTPUT_PREF}.fisher_mutations.txt
-rm -f {OUTPUT_PREF}.realignment_mutations.txt
-rm -f {OUTPUT_PREF}.indel_mutations.txt
-rm -f {OUTPUT_PREF}.breakpoint_mutations.txt
-rm -f {OUTPUT_PREF}.simplerepeat_mutations.txt
+rm -f {OUTPUT_PREF}.fisher_mutations.${{ext}}
+rm -f {OUTPUT_PREF}.realignment_mutations.${{ext}}
+rm -f {OUTPUT_PREF}.indel_mutations.${{ext}}
+rm -f {OUTPUT_PREF}.breakpoint_mutations.${{ext}}
+rm -f {OUTPUT_PREF}.simplerepeat_mutations.${{ext}}
 
 """
 
@@ -100,8 +113,8 @@ def configure(input_bams, gcat_conf, run_conf, sample_conf):
     for (tumor, normal) in sample_conf.genomon_mutation_call:
         output_dir = "%s/genomon_mutation/%s" % (run_conf.project_root, tumor)
         os.makedirs(output_dir, exist_ok=True)
-        output_txt = "%s/%s.genomon_mutation.txt" % (output_dir, tumor)
-        output_files[tumor] = output_txt
+        output_file = "%s/%s.genomon_mutation.result.vcf" % (output_dir, tumor)
+        output_files[tumor] = output_file
         input_normal_cram = ""
         if normal != None:
             input_normal_cram = input_bams[normal]
@@ -118,10 +131,12 @@ def configure(input_bams, gcat_conf, run_conf, sample_conf):
             "INDEL_OPTION": gcat_conf.get(CONF_SECTION, "indel_option"),
             "INDEL_SAMTOOLS_OPTION": gcat_conf.get(CONF_SECTION, "indel_samtools_option"),
             "BREAKPOINT_OPTION": gcat_conf.get(CONF_SECTION, "breakpoint_option"),
+            "SIMPLE_REPEAT_OPTION": gcat_conf.get(CONF_SECTION, "simple_repeat_option"),
             "OUTPUT_PREF": "%s/%s" % (output_dir, tumor),
             "SAMPLE1": tumor,
             "SAMPLE2": normal,
             "ANNOTATION_DB": gcat_conf.get(CONF_SECTION, "annotation_db"),
+            "OUTPUT_FORMAT": gcat_conf.get(CONF_SECTION, "output_format"),
             "FILTER_PAIR_OPTION": gcat_conf.get(CONF_SECTION, "filter_pair_option"),
             "FILTER_SINGLE_OPTION": gcat_conf.get(CONF_SECTION, "filter_single_option"),
         }
