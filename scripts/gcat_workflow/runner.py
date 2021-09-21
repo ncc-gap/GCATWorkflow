fd843a9c232261554b25449ff03dd7db02c877d2
Ai Okada <aokada@ncc.go.jp>
2021-09-21 16:44:19 +0900

    refactor

--- scripts/gcat_workflow/runner.py
+++ scripts/gcat_workflow/runner.py
@@ -20,75 +20,74 @@ class Runner(object):
         pass
 
 class Drmaa_runner(Runner):
-    
     def task_exec(self):
-            import drmaa
-        
-            s = drmaa.Session()
-            s.initialize()
-             
-            jt = s.createJobTemplate()
-            jt.jobName = self.jobname
-            jt.outputPath = ':' + self.log_dir
-            jt.errorPath = ':' + self.log_dir
-            jt.nativeSpecification = self.qsub_option
-            jt.remoteCommand = self.singularity_script
-            os.chmod(self.singularity_script, stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR | stat.S_IRGRP | stat.S_IXGRP)
+        import drmaa
+    
+        s = drmaa.Session()
+        s.initialize()
+         
+        jt = s.createJobTemplate()
+        jt.jobName = self.jobname
+        jt.outputPath = ':' + self.log_dir
+        jt.errorPath = ':' + self.log_dir
+        jt.nativeSpecification = self.qsub_option
+        jt.remoteCommand = self.singularity_script
+        os.chmod(self.singularity_script, stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR | stat.S_IRGRP | stat.S_IXGRP)
 
-            returncode = 0
-            returnflag = True
-            if self.max_task == 0:
-                for var in range(0, (self.retry_count+1)):
-                    jobid = s.runJob(jt)
-                    returncode = 0
-                    returnflag = True
-                    now = datetime.datetime.now()
-                    date = now.strftime("%Y-%m-%d %H:%M:%S")
-                    print ("Your job has been submitted with id: " + jobid + " at Date/Time: " + date)
-                    retval = s.wait(jobid, drmaa.Session.TIMEOUT_WAIT_FOREVER)
+        returncode = 0
+        returnflag = True
+        if self.max_task == 0:
+            for var in range(0, (self.retry_count+1)):
+                jobid = s.runJob(jt)
+                returncode = 0
+                returnflag = True
+                now = datetime.datetime.now()
+                date = now.strftime("%Y-%m-%d %H:%M:%S")
+                print ("Your job has been submitted with id: " + jobid + " at Date/Time: " + date)
+                retval = s.wait(jobid, drmaa.Session.TIMEOUT_WAIT_FOREVER)
+                now = datetime.datetime.now()
+                date = now.strftime("%Y-%m-%d %H:%M:%S")
+                print ("Job: " + str(retval.jobId) + ' finished with status: ' + str(retval.hasExited) + ' and exit status: ' + str(retval.exitStatus) + " at Date/Time: " + date)
+                returncode = retval.exitStatus
+                returnflag = retval.hasExited
+                if returncode == 0 and returnflag: break
+            s.deleteJobTemplate(jt)
+            s.exit()
+
+        else:
+            joblist = s.runBulkJobs(jt,1,self.max_task,1)
+            all_jobids = []
+            for var in range(0, (self.retry_count+1)):
+                if len(all_jobids) > 0:
+                    joblist = all_jobids
+                    all_jobids = []
+                returncode = 0
+                returnflag = True
+                now = datetime.datetime.now()
+                date = now.strftime("%Y-%m-%d %H:%M:%S")
+                print ('Your job has been submitted with id ' + str(joblist) + " at Date/Time: " + date)
+                s.synchronize(joblist, drmaa.Session.TIMEOUT_WAIT_FOREVER, False)
+                for curjob in joblist:
+                    print ('Collecting job ' + curjob)
+                    retval = s.wait(curjob, drmaa.Session.TIMEOUT_WAIT_FOREVER)
                     now = datetime.datetime.now()
                     date = now.strftime("%Y-%m-%d %H:%M:%S")
                     print ("Job: " + str(retval.jobId) + ' finished with status: ' + str(retval.hasExited) + ' and exit status: ' + str(retval.exitStatus) + " at Date/Time: " + date)
-                    returncode = retval.exitStatus
-                    returnflag = retval.hasExited
-                    if returncode == 0 and returnflag: break
-                s.deleteJobTemplate(jt)
-                s.exit()
+                    
+                    if retval.exitStatus != 0 or not retval.hasExited:
+                        returncode = retval.exitStatus
+                        returnflag = retval.hasExited
+                        if var == self.retry_count: break
+                        jobId_list = retval.jobId.split(".")
+                        taskId = int(jobId_list[1])
+                        all_jobids.extend(s.runBulkJobs(jt,taskId,taskId,1))
+                   
+                if returncode == 0 and returnflag: break
+            s.deleteJobTemplate(jt)
+            s.exit()
 
-            else:
-                joblist = s.runBulkJobs(jt,1,self.max_task,1)
-                all_jobids = []
-                for var in range(0, (self.retry_count+1)):
-                    if len(all_jobids) > 0:
-                        joblist = all_jobids
-                        all_jobids = []
-                    returncode = 0
-                    returnflag = True
-                    now = datetime.datetime.now()
-                    date = now.strftime("%Y-%m-%d %H:%M:%S")
-                    print ('Your job has been submitted with id ' + str(joblist) + " at Date/Time: " + date)
-                    s.synchronize(joblist, drmaa.Session.TIMEOUT_WAIT_FOREVER, False)
-                    for curjob in joblist:
-                        print ('Collecting job ' + curjob)
-                        retval = s.wait(curjob, drmaa.Session.TIMEOUT_WAIT_FOREVER)
-                        now = datetime.datetime.now()
-                        date = now.strftime("%Y-%m-%d %H:%M:%S")
-                        print ("Job: " + str(retval.jobId) + ' finished with status: ' + str(retval.hasExited) + ' and exit status: ' + str(retval.exitStatus) + " at Date/Time: " + date)
-                        
-                        if retval.exitStatus != 0 or not retval.hasExited:
-                            returncode = retval.exitStatus
-                            returnflag = retval.hasExited
-                            if var == self.retry_count: break
-                            jobId_list = retval.jobId.split(".")
-                            taskId = int(jobId_list[1])
-                            all_jobids.extend(s.runBulkJobs(jt,taskId,taskId,1))
-                       
-                    if returncode == 0 and returnflag: break
-                s.deleteJobTemplate(jt)
-                s.exit()
-
-            if returncode != 0 or not returnflag: 
-                raise RuntimeError("Job: " + str(retval.jobId)  + ' failed at Date/Time: ' + date)
+        if returncode != 0 or not returnflag: 
+            raise RuntimeError("Job: " + str(retval.jobId)  + ' failed at Date/Time: ' + date)
 
 class Qsub_runner(Runner):
     def task_exec(self):
